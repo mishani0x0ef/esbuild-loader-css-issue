134350f20ac5bc3c361f04e050894d11e826770a
Mykhailo.Romaniuk <mykhailo.romaniuk@eleks.com>
2023-12-27 23:06:23 +0200

    Minimal workable example

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  entry: {
+    index: ["./src/index.ts"],
+  },
+
+  output: {
+    path: __dirname + "/dist",
+    filename: "[name].js",
+  },
+
+  resolve: {
+    extensions: [".ts"],
+  },
+
+  optimization: {
+    minimize: false,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        use: [
+          {
+            loader: "esbuild-loader",
+            options: {
+              target: "es2015",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
